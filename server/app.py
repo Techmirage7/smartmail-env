08dd9311a5d37fe7a3a3ef4f7e22ea3693ab90bf
Techmirage7 <pooravgupta2004@gmail.com>
2026-03-31 10:52:23 +0530

    Add reset and step endpoints for validator

--- server/app.py
+++ server/app.py
@@ -7,8 +7,21 @@ app = FastAPI()
 
 @app.get("/")
 def home():
+    return {"message": "SmartMail Space is running"}
+
+
+@app.post("/reset")
+def reset_env():
     env = SmartMailEnv()
     observation = env.reset()
+
+    return observation.model_dump()
+
+
+@app.post("/step")
+def step_env():
+    env = SmartMailEnv()
+    env.reset()
 
     action = Action(
         action_type="escalate",
@@ -18,7 +31,7 @@ def home():
     observation, reward, done, info = env.step(action)
 
     return {
-        "message": "SmartMail Space is running",
+        "observation": observation.model_dump(),
         "reward": reward,
         "done": done,
         "info": info
